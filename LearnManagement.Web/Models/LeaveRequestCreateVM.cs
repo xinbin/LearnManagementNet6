2fa723034ed4f7a16a364d8da61129c4413ee8c2
Adrianna Flores <adrianna.flores@mortgagelaw.com>
2023-12-01 12:03:34 -0600

    Committing final code changes for section 55. Building out the Employee Create apply for leave code with custom validation

--- LearnManagement.Web/Models/LeaveRequestCreateVM.cs
+++ LearnManagement.Web/Models/LeaveRequestCreateVM.cs
@@ -5,24 +5,38 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LearnManagement.Web.Models
 {
-    public class LeaveRequestCreateVM : IValidatableObject 
+    public class LeaveRequestCreateVM : IValidatableObject
     {
         [Required]
         [Display(Name = "Start Date")]
         public DateTime? StartDate { get; set; }
+
         [Required]
         [Display(Name = "End Date")]
         public DateTime? EndDate { get; set; }
+
         [Required]
+        [Display(Name = "Leave Type")]
         public int LeaveTypeId { get; set; }
 
-        public SelectList LeaveTypes { get; set; }
+        public SelectList? LeaveTypes { get; set; }
+
         [Display(Name = "Comments")]
-        public string RequestComments { get; set; }
+        public string? RequestComments { get; set; }
 
         public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
         {
-            yield return ModelState.AddModelError(string.Empty, "An Error Has Occurred. Please try again later");
+            if (StartDate > EndDate)
+            {
+                yield return new ValidationResult("The Start Date Must Be Before End Date", new[] { nameof(StartDate), nameof(EndDate) });
+
+            }
+
+            // Demonstrates why yield return is used.
+            if (RequestComments?.Length > 250)
+            {
+                yield return new ValidationResult("Comments are too long", new[] { nameof(RequestComments) });
+            }
         }
     }
 }
